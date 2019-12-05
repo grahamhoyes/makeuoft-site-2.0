41a4c9bf7b19de71288a8cbc0e2938880d0440c6
Graham Hoyes <accounts@grahamhoyes.com>
2019-12-04 19:51:26 -0500

    More black formatting

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,41 +1,62 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateField, SelectField, TextAreaField
+from wtforms import (
+    StringField,
+    PasswordField,
+    SubmitField,
+    BooleanField,
+    DateField,
+    SelectField,
+    TextAreaField,
+)
 from wtforms.widgets import ListWidget, CheckboxInput
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from application.db_models import Users
 
+
 class LoginForm(FlaskForm):
-    email = StringField('E-Mail', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    email = StringField("E-Mail", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
+
 
 class RegistrationForm(FlaskForm):
     """
     Registration that allows customers to register with their first name,
     last name, email and a password
     """
+
     first_name = StringField("First Name", validators=[DataRequired()])
     last_name = StringField("Last Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
-    submit = SubmitField('Register')
+    password2 = PasswordField(
+        "Repeat Password",
+        validators=[
+            DataRequired(),
+            EqualTo("password", message="Passwords must match."),
+        ],
+    )
+    submit = SubmitField("Register")
 
     def validate_email(self, email):
         search_email = Users.query.filter_by(email=email.data).first()
-        if(search_email is not None):
-            raise ValidationError('Please use a different email address.')
+        if search_email is not None:
+            raise ValidationError("Please use a different email address.")
+
 
 class ApplicationForm(FlaskForm):
     """
     Application form that takes all of the information of contestants
     and puts it into the database
     """
+
     first_name = StringField("First Name", validators=[DataRequired()])
     last_name = StringField("Last Name", validators=[DataRequired()])
     birthday = DateField("Birthday", validators=[DataRequired()])
-    gender = SelectField("Gender", choices=[('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')])
+    gender = SelectField(
+        "Gender", choices=[("Male", "Male"), ("Female", "Female"), ("Other", "Other")]
+    )
     ethnicity = StringField("Ethnicity", validators=[DataRequired()])
 
     email = StringField("Email", validators=[DataRequired(), Email()])
@@ -53,15 +74,17 @@ class ApplicationForm(FlaskForm):
 
     how_you_hear = TextAreaField("How You Hear", validators=[DataRequired()])
 
-    submit = SubmitField('Register')
+    submit = SubmitField("Register")
 
     def validate_email(self, email):
         search_email = Users.query.filter_by(email=email.data).first()
-        if(search_email is not None):
-            raise ValidationError('Please use a different email address.')
+        if search_email is not None:
+            raise ValidationError("Please use a different email address.")
+
 
 class ForgotPasswordEmailForm(FlaskForm):
     """
     Form takes email of contestant when they forgot their password
     """
+
     email = StringField("Email", validators=[DataRequired(), Email()])
